c3d1fcdffc66bd6a717e50a7ad5806baac2b2ef0
kangbo1229 <kangbo@ssfs.org>
2020-03-09 14:16:03 -0400

    Initial commit

---
+++ src/ThreeByFive.java
@@ -0,0 +1,34 @@
+public class ThreeByFive {
+    public static void main(String [] args) {
+        int[][] nums = { {3,6,2,9,7}, {7,45,9,12,44}, {2,33,5,88,0}};
+
+        System.out.println(nums[0][0]);
+        System.out.println(nums[0][1]);
+        System.out.println(nums[0][2]);
+        System.out.println(nums[0][3]);
+        System.out.println(nums[0][4]);
+        System.out.println(nums[1][0]);
+        System.out.println(nums[1][1]);
+        System.out.println(nums[1][2]);
+        System.out.println(nums[1][3]);
+        System.out.println(nums[1][4]);
+        System.out.println(nums[2][0]);
+        System.out.println(nums[2][1]);
+        System.out.println(nums[2][2]);
+        System.out.println(nums[2][3]);
+        System.out.println(nums[2][4]);
+
+//change three other values to negative numbers
+        nums[0][3] = -6;
+        nums[1][4] = -4;
+        nums[2][2] = -5;
+
+//make a statement to create a blank line here
+
+// output all of the numbers again,
+// not just the changed ones
+        System.out.println(nums[1][4]);
+    }
+}
+
+
